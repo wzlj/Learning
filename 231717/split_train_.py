ab4bfc354b6aef37153b2b380117bbc525e7c81e
wzlj <46277043+wzlj@users.noreply.github.com>
2019-07-07 23:04:44 +0800

    Update split_train_.py

--- 231717/split_train_.py
+++ 231717/split_train_.py
@@ -4,12 +4,14 @@ import numpy as np
 
 Image.MAX_IMAGE_PIXELS = 100000000000
 np.set_printoptions(suppress=True)
-np.set_printoptions(threshold=np.nan)
-def crop_image(number, size=256):
+np.set_printoptions(threshold=np.inf)
+
+
+def crop_image(number, size=256, ratio=0.7):
     #
-    imagepath = '../data/jingwei_round1_train_20190619/image_{}.png'.format(number)
+    imagepath = '../origin_data/train_data/image_{}.png'.format(number)
     n = os.path.basename(imagepath)[:-4]
-    labelname = '../data/jingwei_round1_train_20190619/' + n + '_label.png'
+    labelname = '../origin_data/train_data/' + n + '_label.png'
     img_label = Image.open(labelname)
     img = Image.open(imagepath)
 
@@ -18,10 +20,10 @@ def crop_image(number, size=256):
     print(img.shape)
     print(img_label.shape)
 
-    width, height, _ = img.shape
+    height, width, _ = img.shape
     unit_size = size
-    x1, x2, y1, y2 = 0, unit_size, 0, unit_size
-    nullthresh = unit_size * unit_size * 0.3
+    y1, y2, x1, x2 = 0, unit_size, 0, unit_size
+    nullthresh = unit_size * unit_size * (1 - ratio)
 
     print(x1, x2, y1, y2)
 
@@ -34,8 +36,8 @@ def crop_image(number, size=256):
             if y2 > height:
                 y2, y1 = height, height - unit_size
 
-            _img = img[x1:x2, y1:y2, :3]
-            _img_label = img_label[x1:x2, y1:y2]
+            _img = img[y1:y2, x1:x2, :3]
+            _img_label = img_label[y1:y2, x1:x2]
             if (_img_label[:, :] == 0).sum() > nullthresh:
                 y1 += unit_size
                 y2 += unit_size
@@ -45,21 +47,21 @@ def crop_image(number, size=256):
             print("\n\n shape of _img_label is  ", _img_label.shape)
             print("\n\n shape of _img is  ", _img.shape)
             _img = Image.fromarray(np.uint8(_img))
-            _img.save('../data/train/data512/' + n + '_{}_{}.png'.format(x1, y1))
-
+            dst_path = '../data/train/data{}_{}/'.format(size, ratio)
+            if not os.path.exists(dst_path):
+                os.mkdir(dst_path)
+            data_path = os.path.join(dst_path, n+'_{}_{}.png'.format(x1, y1))
+            _img.save(data_path)
 
             print(_img_label.shape)
             _img_label = Image.fromarray(_img_label).convert('L')
             print(_img_label.size)
+            dst_path = '../data/train/label{}_{}/'.format(size, ratio)
+            if not os.path.exists(dst_path):
+                os.mkdir(dst_path)
+            label_path = os.path.join(dst_path, n+'_{}_{}.png'.format(x1, y1))
+            _img_label.save(label_path)
 
-            _img_label.save('../data/train/label512/' + n + '_{}_{}.png'.format(x1, y1))
-
-            # img2 = Image.fromarray(_img, 'RGB')
-            # img2.save('../data/train/data512/' + n + '_{}_{}.bmp'.format(x1, y1))
-            # img = Image.fromarray(_img_label).convert('L')
-            # img.save('../data/train/label512/' + n + '_{}_{}.bmp'.format(x1, y1))
-
-            # name = input("input the image number 1 or 2 you want clip:")
             y1 += unit_size
             y2 += unit_size
         if x2 == width:
@@ -71,7 +73,4 @@ def crop_image(number, size=256):
 
 if __name__ == '__main__':
     for i in range(1, 3):
-        crop_image(i, size=512)
-
-
-
+        crop_image(i, size=1024, ratio=0.7)
